3e64f5b1d2338b026eeb9c9e23b7850bf6efb232
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-06-28 16:05:05 +0300

    Add files via upload
    add task1

---
+++ task1/task1.py
@@ -0,0 +1,10 @@
+m = list(range(1, 1+int(input('введите длину массива '))))
+n = int(input('введите шаг массива '))
+i, result = 0, []
+while True:
+    result.append(m[i:i+n][0])
+    print(m[i:i+n]+m[:n-len(m[i:i+n])])
+    i = (i+n)%len(m)-bool((i+n)%len(m))
+    if m[i:i+n][0] == m[0]:
+        print('\nresult = ',result)
+        break

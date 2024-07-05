de34d3b481f893aed7177a792f6186659bcedfd7
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-07-05 18:30:08 +0300

    Update task1.py
    fix result

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,14 @@
 def task1(m, n):
     i, result = 0, []
+    print('M', m)
     while True:
         result.append(m[i:i+n][0])
         print(m[i:i+n]+m[:n-len(m[i:i+n])])
+        l = m[i:i+n]+m[:n-len(m[i:i+n])]
         i = (i+n)%len(m)-bool((i+n)%len(m))
-        if m[i:i+n][0] == m[0]:
-            result.append(m[-1])     
+        if m[i:i+n][0] == 1:
+            if l[-1]!=1:
+                result.append(m[-1])            
             print('\nresult = ',result)
             break
         

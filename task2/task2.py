122c4a1b5f720c3fcd10f747ed4dea3ef94ee8b9
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-07-05 17:55:00 +0300

    Update task2.py
    fix input

--- task2/task2.py
+++ task2/task2.py
@@ -1,15 +1,19 @@
-from circle1 import x, y, r
-from coordinates2 import dot_coords
+import importlib 
+def dot_detection(mod_c, mod_coor):
 
-def dot_detection():
-    for dot in dot_coords:
-        relative_x, relative_y = x - dot[0], y - dot[1]
+    for dot in mod_coor.dot_coords:
+        relative_x, relative_y = mod_c.x - dot[0], mod_c.y - dot[1]
         hypotenuse = (relative_x**2+relative_y**2)**(1/2)
-        if hypotenuse < r:
+        if hypotenuse < mod_c.r:
             print(1)
-        elif hypotenuse > r:
+        elif hypotenuse > mod_c.r:
             print(2)
         else:
             print(0)
         
-dot_detection()
+if __name__ == '__main__': 
+    circle_file = input('Введите фаил значений окружности: ')
+    coordinates_file  = input('Введите фаил значений координат: ')
+    mod_c = __import__(circle_file)
+    mod_coor = __import__(coordinates_file)
+    dot_detection(mod_c, mod_coor)

ee81fcba43778db40e37996066bd355d22d9fe2d
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-07-05 17:54:22 +0300

    Update task1.py
    fix input

--- task1/task1.py
+++ task1/task1.py
@@ -1,12 +1,15 @@
-m = list(range(1, 1+int(input('введите длину массива '))))
-n = int(input('введите шаг массива '))
-i, result = 0, []
-while True:
-    result.append(m[i:i+n][0])
-    print(m[i:i+n]+m[:n-len(m[i:i+n])])
-    i = (i+n)%len(m)-bool((i+n)%len(m))
-    if m[i:i+n][0] == m[0]:
-        if len(m)%2!=0:
+def task1(m, n):
+    i, result = 0, []
+    while True:
+        result.append(m[i:i+n][0])
+        print(m[i:i+n]+m[:n-len(m[i:i+n])])
+        i = (i+n)%len(m)-bool((i+n)%len(m))
+        if m[i:i+n][0] == m[0]:
             result.append(m[-1])     
-        print('\nresult = ',result)
-        break
+            print('\nresult = ',result)
+            break
+        
+if __name__ == '__main__':        
+    m = list(range(1, 1+int(input('введите длину массива '))))
+    n = int(input('введите шаг массива '))
+    task1(m, n)

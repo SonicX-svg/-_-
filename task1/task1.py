c5f3437acbb7b780934f391a9b444bd65caf817a
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-07-11 08:44:43 +0300

    Update task1.py
    fix inpt

--- task1/task1.py
+++ task1/task1.py
@@ -1,3 +1,5 @@
+import sys
+
 def task1(m, n):
     i, result = 0, []
     print('M', m)
@@ -8,11 +10,15 @@ def task1(m, n):
         i = (i+n)%len(m)-bool((i+n)%len(m))
         if m[i:i+n][0] == 1:
             if l[-1]!=1:
-                result.append(m[-1])            
+                result.append(m[-1])    
+             
             print('\nresult = ',result)
             break
         
-if __name__ == '__main__':        
-    m = list(range(1, 1+int(input('введите длину массива '))))
-    n = int(input('введите шаг массива '))
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python task1.py <m> <n>")
+        sys.exit(1)
+    m = list(range(1, 1+int(sys.argv[1])))
+    n = int(sys.argv[2])
     task1(m, n)

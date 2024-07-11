8a040041037da7a284898b7d7f885023d8f54b16
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-07-11 08:45:39 +0300

    Update task2.py
    fix inpt

--- task2/task2.py
+++ task2/task2.py
@@ -1,6 +1,6 @@
+import sys
 
 def dot_detection(mod_c, mod_coor):
-
     for dot in mod_coor.dot_coords:
         relative_x, relative_y = mod_c.x - dot[0], mod_c.y - dot[1]
         hypotenuse = (relative_x**2+relative_y**2)**(1/2)
@@ -12,8 +12,11 @@ def dot_detection(mod_c, mod_coor):
             print(0)
         
 if __name__ == '__main__': 
-    circle_file = input('Введите фаил значений окружности: ')
-    coordinates_file  = input('Введите фаил значений координат: ')
-    mod_c = __import__(circle_file)
-    mod_coor = __import__(coordinates_file)
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <фаил окружность> <фаил точек> without .py")
+        sys.exit(1)
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    mod_c = __import__(arg1)
+    mod_coor = __import__(arg2)
     dot_detection(mod_c, mod_coor)
